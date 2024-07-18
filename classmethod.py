587271378f215eaf9a64be939ff3ae54c2ad2b01
Альберт Акопян <163412031+albertgrayce01@users.noreply.github.com>
2024-07-18 19:14:27 +0300

    Initial commit

---
+++ classmethod.py
@@ -0,0 +1,22 @@
+class Vector:
+    MIN_COORD = 0
+    MAX_COORD = 100
+
+    @classmethod
+    def validate(cls, arg):
+        return cls.MIN_COORD <= arg <= cls.MAX_COORD
+
+    def __init__(self, x, y):
+        self.x = self.y = 0
+        if self.validate(x) and self.validate(y):
+            self.x = x
+            self.y = y
+
+    def get_coord(self):
+        return self.x, self.y
+
+
+v = Vector(1, 200)
+print(Vector.validate(5))
+res = Vector.get_coord(v)
+print(res)
